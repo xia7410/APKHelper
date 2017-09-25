32f4b13a529e214724a51be724b4043c67b60957
Harbour <935468819@qq.com>
2017-09-25 15:54:54 +0800

    加入nuget

--- APKHelper/Properties/AssemblyInfo.cs
+++ APKHelper/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("APKHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("APKHelper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Harbour")]
 [assembly: AssemblyProduct("APKHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Harbour")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
